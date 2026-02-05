07b7d28613772aec7ee22b99e23f22a73bbb07a8
Tim de Klijn <tim.de.klijn@gmail.com>
2026-02-05 14:25:30 +0100

    update DAG

--- dags.py
+++ dags.py
@@ -1,3 +1,31 @@
-from first_project import run
+from datetime import datetime
 
-run.runner()
+from airflow import DAG
+from kubernetes.client import models as k8s
+from airflow.providers.cncf.kubernetes.operators.pod import KubernetesPodOperator
+
+with DAG(
+    dag_id="first_project",
+    start_date=datetime(2024, 1, 1),
+    schedule=None,
+    catchup=False,
+    default_args={
+        "owner": "tim",
+        "retries": 0,
+    },
+    tags=["first_project", "team:TIM"],
+) as dag:
+    run_ml_task = KubernetesPodOperator(
+        task_id="first_ingest",
+        name="first-ingest",
+        image="first_project:latest",
+        namespace="airflow-poc",
+        arguments=["ingest"],
+        get_logs=True,
+        is_delete_operator_pod=True,
+        resources=k8s.V1ResourceRequirements(
+            requests={"memory": "1Gi", "cpu": "500m"},
+            limits={"memory": "2Gi", "cpu": "1000m"},
+        ),
+        labels={"project": "first_project", "team": "TIM"},
+    )
